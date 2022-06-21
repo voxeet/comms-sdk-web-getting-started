32cc45873edfd7afe1514f829c5d048446002930
j12y <jayson.delancey@gmail.com>
2022-06-21 08:46:59 -0700

    New Getting Started Guide Web SDK Starter Project

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
-# Dolby.io Communications SDK for Web - Getting Started app
+# Dolby.io Communications Web SDK
 
-This is the sample app from the browser app used in the
-[Getting Started](https://docs.dolby.io/communications-apis/docs/create-a-basic-audio-conference-application) article. 
+This starter project demonstrates the mechanics of making a Voice and Video Call with Dolby.io.
 
-You can find additional reference documentation here:
-- [JavaScript Reference](https://docs.dolby.io/communications-apis/docs/js-client-sdk-voxeetsdk)
+## Getting Started
+
+Follow the [Getting Started](https://docs.dolby.io/communications-apis/docs/getting-started-with-the-javascript-sdk) tutorial to learn how this application works.
 
 ## Running
 
-See [this page](https://docs.dolby.io/communications-apis/docs/create-a-basic-audio-conference-application#run-your-application)
-for instructions on how to run the app.
+This project runs as a single-page application with any web server.
+
+You can run a web server in a Node.js environment with `live-server` or `http-server`:
+```
+npx http-server
+```
+
+You can run a web server in a Python 3 environment with `http.server`:
+```
+python3 -m http.server
+```
+
+You can also explore static site hosting and traditional HTTP servers.
+
+## Troubleshooting
+
+See the accompanying [Web SDK Troubleshooting](https://docs.dolby.io/communications-apis/docs/getting-started-with-the-javascript-sdk#troubleshooting)
+
+## Learn More
+
+For more information, visit the [Dolby.io Communications Web SDK](https://docs.dolby.io/communications-apis/docs/js-overview) documentation.
---
+++ video-calls/util/dolbyio-auth-helper.js
@@ -0,0 +1,76 @@
+const dolbyio = {};
+
+/**
+ * Open a modal to allow user to paste in a token. This helper
+ * only works with the starter project that has the elements
+ * defined.
+ *
+ * @param {*}
+ * @returns token (string)
+ */
+dolbyio.tokenPrompt = () => {
+	document.getElementById('btn-token').onclick = async () => {
+		modal.hide();
+
+		let token = document.getElementById('input-token').value;
+		let params = new URLSearchParams(window.location.search);
+		params.set("token", token);
+		window.location.search = params.toString();
+
+		return token;
+	};
+
+	const modalElement = document.getElementById('token-prompt');
+	const modal = new bootstrap.Modal(modalElement, {
+		backdrop: 'static',
+		keyboard: false,
+		focus: true
+	});
+	modal.show();
+}
+
+/**
+ * Return an access token for initializing the Dolby.io Communications
+ * Web SDK. This method is looking for a token to be provided as a
+ * querytring parameter or will prompt with a modal dialog for manual
+ * entry.
+ *
+ * @param {*}
+ * @returns token (string)
+ *
+ * This function will help provide a token that is given to the application
+ * as a querystring parameter.
+ */
+dolbyio.getAccessToken = () => {
+    // For an insecure but quick test, you can return a hard-coded
+    // access token during development.
+    // return '<REPLACE-WITH-TEMPORARY-ACCESS-TOKEN>';
+
+	const queryParams = new URLSearchParams(window.location.search);
+
+	console.group('Dolby.io Client Access Token');
+	const accessToken = queryParams.get('token');
+
+	if (!accessToken) {
+		let inputToken = dolbyio.tokenPrompt();
+		console.log(inputToken);
+	}
+
+    try {
+        const token = accessToken.split('.')[1];
+        const jwt = JSON.parse(window.atob(token));
+        const expiration = new Date(jwt.exp * 1000);
+
+        console.log(`Token: ${accessToken}`);
+        console.log(`Expires: ${expiration}`);
+        if (expiration.getTime() <= new Date().getTime()) {
+            console.log("This token has expired.  Fetch a new one from the Dolby.io dashboard.");
+        }
+    } catch(error) {
+        console.error(error);
+    }
+	console.groupEnd();
+
+	return accessToken;
+};
+
